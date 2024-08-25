6c1e84b388757279ecf2400472e634dcfc06fa85
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-08-25 19:39:32 +0500

    Posts and Post component ui

---
+++ client/src/components/post/Post.jsx
@@ -0,0 +1,51 @@
+
+import './post.scss'
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
+import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { Link } from "react-router-dom";
+
+const Post = ({post}) => {
+    //temporary
+    const liked = false;
+    return (
+    <div className='post'>
+        <div className="container">
+        <div className="user">
+            <div className="userInfo">
+                <img src={post.profilePic} alt="" />
+                <div className="details">
+                    <Link to={`/profile/${post.userId}`} style={{textDecoration:'none', color:'inherit'}}>
+                    <span className='name'>{post.name}</span>
+                    </Link>
+                    <span className='date'>1 min ago</span>
+                </div>
+            </div>
+            <MoreHorizIcon />
+        </div>
+        <div className="content">
+            <p>{post.desc}</p>
+            {post.img && <img src={post.img} alt='image' />}
+        </div>
+        <div className="info">
+            <div className="item">
+                {liked ? <FavoriteOutlinedIcon />: <FavoriteBorderOutlinedIcon/>}
+                <p>131 likes</p>
+            </div>
+            <div className="item">
+               <TextsmsOutlinedIcon />
+                <p>21 Comments</p>
+            </div>
+            <div className="item">
+                <ShareOutlinedIcon />
+                    <p>Share</p>
+            </div>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Post
--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,8 +1,50 @@
-import './posts.scss'
+import Post from "../post/Post";
+import "./posts.scss";
+
+//temporary post datas
+const posts = [
+  {
+    id: 1,
+    name: "Alice Johnson",
+    userId: 101,
+    profilePic: "https://randomuser.me/api/portraits/women/44.jpg",
+    img: "https://images.pexels.com/photos/707344/pexels-photo-707344.jpeg?auto=compress&cs=tinysrgb&w=600",
+    desc: "Exploring the beauty of nature. #nature #adventure",
+  },
+  {
+    id: 2,
+    name: "Michael Smith",
+    userId: 102,
+    profilePic: "https://randomuser.me/api/portraits/men/32.jpg",
+    img: "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0",
+    desc: "City lights and late nights. #cityscape #urbanlife",
+  },
+  {
+    id: 3,
+    name: "Jessica Williams",
+    userId: 103,
+    profilePic: "https://randomuser.me/api/portraits/women/65.jpg",
+    img: "https://images.unsplash.com/photo-1519337265831-281ec6cc8514",
+    desc: "Cozy winter vibes. #winter #cozy",
+  },
+  {
+    id: 4,
+    name: "David Brown",
+    userId: 104,
+    profilePic: "https://randomuser.me/api/portraits/men/45.jpg",
+    img: "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0",
+    desc: "Weekend getaway in the mountains. #travel #mountains",
+  },
+];
+
 const Posts = () => {
   return (
-    <div>Posts</div>
-  )
-}
+    <div className="posts">
+      {posts.map((post) => (   
+          <Post post={post}  key={post.id}/> 
+      ))}
+    </div>
+  );
+};
 
-export default Posts
+export default Posts;
