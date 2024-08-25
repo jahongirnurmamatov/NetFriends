ad63475dd98bd93abd9d37978f924ab613854f2f
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-08-25 15:56:59 +0500

    rightbar ui

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -1,10 +1,130 @@
-
-import './rightbar.scss';
+import "./rightbar.scss";
 
 const Rightbar = () => {
   return (
-    <div className='rightbar'>Rightbar</div>
-  )
-}
+    <div className="rightbar">
+      <div className="container">
+        {/* first item */}
+        <div className="item">
+          <span>Suggested for you</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <span>Jane Doe</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <span>Jane Doe</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+        </div>
 
-export default Rightbar
+        {/* second item */}
+        <div className="item">
+          <span>Latest activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <p>
+                <span>Jane Doe </span>
+                changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <p>
+                <span>Austin Mark </span>
+                posted a new photo
+              </p>
+            </div>
+            <span>2 h ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/1499327/pexels-photo-1499327.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <p>
+                <span>Nancy Bay </span>
+                shared a new post
+              </p>
+            </div>
+            <span>1 d ago</span>
+          </div>
+        </div>
+        {/* third item  online users*/}
+        <div className="item">
+          <span>Online friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <div className="online"/>
+              <span>Jane Doe </span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <div className="online"/>
+              <span>Jane Doe </span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <div className="online"/>
+              <span>Jane Doe </span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <div className="online"/>
+              <span>Jane Doe </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Rightbar;
