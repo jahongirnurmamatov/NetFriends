ce4ce3c87275fa120fbe1c35b9e42f6c5a7502d6
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-08-26 10:41:53 +0500

    login api endpoint

--- api/index.js
+++ api/index.js
@@ -4,12 +4,15 @@ import postsRoutes from './routes/posts.js'
 import likesRoutes from './routes/likes.js'
 import authRoutes from './routes/auth.js'
 import commentsRoutes from './routes/comments.js'
-
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 
 //middlewares
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 
 app.use('/api/users',userRoutes);
