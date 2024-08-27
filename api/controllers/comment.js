df7d0ffe9548cc1752206ca5965ce857e0c72815
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-08-27 13:11:05 +0500

    comment functionality

--- api/controllers/comment.js
+++ api/controllers/comment.js
@@ -1,4 +1,6 @@
 import { db } from "../connect.js";
+import jwt from 'jsonwebtoken';
+import moment from 'moment';
 
 export const getComments = (req, res) => {
   const q = `SELECT c.*, u.id AS userId,name,profilePic FROM social.comments AS c JOIN social.users AS u ON (u.id = c.userId)
@@ -9,5 +11,28 @@ export const getComments = (req, res) => {
   db.query(q, [req.query.postId], (err, data) => {
     if (err) return res.status(500).json("Interal server error");
     return res.status(200).json(data);
+  });
+};
+
+export const addComments = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in");
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Not valid token");
+
+    const q =
+      "INSERT INTO social.comments (`desc`, `userId`, `createdAt`, `postId`) VALUES (?)";
+
+    const values = [
+      req.body.desc,
+      userInfo.id,
+      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+      req.body.postId
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json("Interal server error");
+      return res.status(200).json("Comment has been created");
+    });
   });
 };
--- client/src/components/comments/Comments.jsx
+++ client/src/components/comments/Comments.jsx
@@ -1,30 +1,50 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./comments.scss";
 import { AuthContext } from "../../context/authContext";
 import { makeRequest } from "../../axios";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import moment from "moment";
 
-const Comments = ({postId}) => {
+const Comments = ({ postId }) => {
   const { currentUser } = useContext(AuthContext);
+  const [desc, setDesc] = useState("");
   const { isLoading, error, data } = useQuery({
     queryKey: ["comments"],
     queryFn: () =>
       makeRequest.get("/comments?postId=" + postId).then((res) => res.data),
   });
-  console.log(postId)
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: (newComment) => makeRequest.post("/comments", newComment),
+    onSuccess: () => {
+      setDesc("");
+      queryClient.invalidateQueries(["comments"]);
+    },
+  });
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    mutation.mutate({ desc, postId });
+    setDesc("");
+  };
 
   return (
     <div className="comments">
       <div className="write">
         <img src={currentUser.profilePic} alt="" />
-        <input type="text" placeholder="Comment here" />
-        <button>Send</button>
+        <input
+          type="text"
+          placeholder="Comment here"
+          onChange={(e) => setDesc(e.target.value)}
+        />
+        <button onClick={handleClick}>Send</button>
       </div>
       {isLoading
         ? "Loading..."
         : data.map((comment) => (
-            <div className="comment"  key={comment.id}>
+            <div className="comment" key={comment.id}>
               <img src={comment.profilePic} alt="" />
               <div className="info">
                 <span>{comment.name}</span>
